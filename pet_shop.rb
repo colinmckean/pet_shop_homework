81a29482e7d30f982a2cf1cbdd7c0c2bc9ae2e82
Colin McKean <colinmckeandigital@gmail.com>
2017-01-13 12:43:07 +0000

    complete test_add_pet_to_customer

--- pet_shop.rb
+++ pet_shop.rb
@@ -19,13 +19,13 @@ def increase_pets_sold(pet_shop_hash, number_of_pets)
 end
 
 def stock_count(pet_shop_hash)
-	return pet_shop_hash[:pets].count
+	return pet_shop_hash[:pets].count()
 end
 
 def pets_by_breed(pet_shop_hash, pet_breed_string)
 	pets_array = []
 	for pet in pet_shop_hash[:pets]
-		if pet[:breed] == pet_breed_string
+		if(pet[:breed] == pet_breed_string)
 			pets_array.push(pet)
 		end
 	end
@@ -34,7 +34,7 @@ end
 
 def find_pet_by_name(pet_shop_hash, pet_name_string)
 	for pet in pet_shop_hash[:pets]
-		if pet[:name] == pet_name_string
+		if(pet[:name] == pet_name_string)
 			return pet
 		end
 	end
@@ -43,7 +43,7 @@ end
 
 def remove_pet_by_name(pet_shop_hash, pet_name_string)
 	for pet in pet_shop_hash[:pets]
-		if pet[:name] == pet_name_string
+		if(pet[:name] == pet_name_string)
 			pet_shop_hash[:pets].delete(pet)
 		end
 	end
@@ -53,3 +53,10 @@ def add_pet_to_stock(pet_shop_hash, pet_to_add_hash)
 	pet_shop_hash[:pets].push(pet_to_add_hash)
 end
 
+def customer_pet_count(customer_hash)
+	return customer_hash[:pets].length()
+end
+
+def add_pet_to_customer(customer_hash, new_pet_hash)
+	customer_hash[:pets].push(new_pet_hash)
+end
